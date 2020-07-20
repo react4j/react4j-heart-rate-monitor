f15be63269f3c50e96a270d65c4295cb0ad26a72
Peter Donald <peter@realityforge.org>
2020-07-20 19:50:14 +1000

    Initial application placeholder code

---
+++ src/main/java/react4j/hrm/Main.java
@@ -0,0 +1,15 @@
+package react4j.hrm;
+
+import com.google.gwt.core.client.EntryPoint;
+import elemental2.dom.DomGlobal;
+import react4j.dom.ReactDOM;
+
+public final class Main
+  implements EntryPoint
+{
+  @Override
+  public void onModuleLoad()
+  {
+    ReactDOM.render( ApplicationBuilder.build(), DomGlobal.document.getElementById( "app" ) );
+  }
+}
