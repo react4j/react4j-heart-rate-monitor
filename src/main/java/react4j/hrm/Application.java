f1eba0654b614abb20ced0dce4eb52fd546ad9e9
Peter Donald <peter@realityforge.org>
2020-07-21 10:32:00 +1000

    Experimental UI

--- src/main/java/react4j/hrm/Application.java
+++ src/main/java/react4j/hrm/Application.java
@@ -1,10 +1,17 @@
 package react4j.hrm;
 
 import arez.annotations.CascadeDispose;
+import arez.annotations.Feature;
+import arez.annotations.Observable;
+import elemental2.dom.DomGlobal;
 import javax.annotation.Nonnull;
+import jsinterop.base.Js;
 import react4j.ReactNode;
 import react4j.annotations.Render;
 import react4j.annotations.View;
+import react4j.dom.events.ReactEventHandler;
+import react4j.dom.proptypes.html.BtnProps;
+import react4j.dom.proptypes.html.CssProps;
 import react4j.dom.proptypes.html.HtmlProps;
 import react4j.dom.proptypes.html.ImgProps;
 import static react4j.dom.DOM.*;
@@ -15,19 +22,65 @@ abstract class Application
   @Nonnull
   @CascadeDispose
   final HeartRateMonitor _heartRateMonitor = HeartRateMonitor.create();
+  double _animationDuration;
+
+  @Observable( readOutsideTransaction = Feature.ENABLE, writeOutsideTransaction = Feature.ENABLE )
+  abstract boolean isExpandAnimation();
+
+  abstract void setExpandAnimation( boolean expandAnimation );
 
   @Nonnull
   @Render
   ReactNode render()
   {
+    final CssProps cssProps = new CssProps();
+
     final int heartRate = _heartRateMonitor.getHeartRate();
+    if ( heartRate > 0 )
+    {
+      if( 0 == _animationDuration )
+      {
+        // We used to try and change the animated duration but this property
+        // can not be used in transition and changing it in onAnimationIteration is
+        // seemingly too late and causes animation to reset
+        _animationDuration = ( (int) ( 60.0 * 100.0 / heartRate ) ) / 100.0;
+      }
+      cssProps
+        .animationName( "beating" )
+        .animationDirection( "alternate" )
+        .animationIterationCount( "infinite" );
+      //TODO: Add mapping for this into react4j
+      Js.asPropertyMap( cssProps ).set( "animationTimingFunction", "ease-in" );
+      //TODO: Add mapping for this into react4j
+      Js.asPropertyMap( cssProps ).set( "animationDuration", _animationDuration + "s" );
+    }
+    else
+    {
+      _animationDuration = 0;
+    }
     final boolean connected = _heartRateMonitor.isConnected();
+    final ImgProps imgProps = new ImgProps()
+      .className( "heart" )
+      .style( cssProps )
+      .src( "img/heart.svg" );
+
+    //TODO: Add react4j support for onAnimationStart onAnimationEnd onAnimationIteration
+    final ReactEventHandler onAnimationStart = e -> DomGlobal.console.log( e.getNativeEvent() );
+    imgProps.prop( "onAnimationStart", Js.asAny( onAnimationStart ) );
+    final ReactEventHandler onAnimationEnd = e -> DomGlobal.console.log( e.getNativeEvent() );
+    imgProps.prop( "onAnimationEnd", Js.asAny( onAnimationEnd ) );
+    final ReactEventHandler onAnimationIteration = e -> {
+      DomGlobal.console.log( e.getNativeEvent() );
+      setExpandAnimation( !isExpandAnimation() );
+    };
+    imgProps.prop( "onAnimationIteration", Js.asAny( onAnimationIteration ) );
     return
       div( new HtmlProps().className( "container" ),
            div( new HtmlProps().className( "hrm_panel" ),
                 h1( "Heart Rate Monitor" ),
-                img( new ImgProps().className( "heart" ).src( "img/heart.svg" ) ),
+                img( imgProps ),
                 0 != heartRate ? ReactNode.of( heartRate ) : null,
+                0 != heartRate ? ( isExpandAnimation() ? ReactNode.of( " Expand" ) : ReactNode.of( " Contract" ) ) : null,
                 button( new BtnProps()
                           .disabled( connected )
                           .onClick( e -> _heartRateMonitor.connect() ),
