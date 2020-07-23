6270c010a78f46194171ca04ce8f3fe5718e86c8
Peter Donald <peter@realityforge.org>
2020-07-23 17:26:45 +1000

    Regenerate code

--- src/main/java/elemental3/BluetoothUUID.java
+++ src/main/java/elemental3/BluetoothUUID.java
@@ -1,6 +1,7 @@
 package elemental3;
 
 import javax.annotation.Generated;
+import javax.annotation.Nonnull;
 import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsType;
 
@@ -17,4 +18,25 @@ public class BluetoothUUID {
   @Deprecated
   BluetoothUUID() {
   }
+
+  @Nonnull
+  public static native String canonicalUUID(int alias);
+
+  @Nonnull
+  public static native String getCharacteristic(@Nonnull String name);
+
+  @Nonnull
+  public static native String getCharacteristic(int name);
+
+  @Nonnull
+  public static native String getDescriptor(@Nonnull String name);
+
+  @Nonnull
+  public static native String getDescriptor(int name);
+
+  @Nonnull
+  public static native String getService(@Nonnull String name);
+
+  @Nonnull
+  public static native String getService(int name);
 }
