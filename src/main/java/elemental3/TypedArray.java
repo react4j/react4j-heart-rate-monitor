23a433385dbcfdb288dc394265eb7f98847aa2c0
Peter Donald <peter@realityforge.org>
2020-11-28 20:30:00 +1100

    Add missed files

---
+++ src/main/java/elemental3/TypedArray.java
@@ -0,0 +1,15 @@
+package elemental3;
+
+import elemental3.core.ArrayBufferView;
+import javax.annotation.Generated;
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsType;
+
+@Generated("org.realityforge.webtack")
+@JsType(
+    isNative = true,
+    namespace = JsPackage.GLOBAL,
+    name = "?"
+)
+public interface TypedArray extends ArrayBufferView {
+}
---
+++ src/main/java/elemental3/core/ArrayBufferView.java
@@ -0,0 +1,15 @@
+package elemental3.core;
+
+import elemental3.BufferSource;
+import javax.annotation.Generated;
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsType;
+
+@Generated("org.realityforge.webtack")
+@JsType(
+    isNative = true,
+    namespace = JsPackage.GLOBAL,
+    name = "?"
+)
+public interface ArrayBufferView extends BufferSource {
+}
