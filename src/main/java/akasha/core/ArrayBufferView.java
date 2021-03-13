63b8841c1f391c1ef3086f479210bf9b1e3cbd7f
Peter Donald <peter@realityforge.org>
2021-03-14 08:31:26 +1100

    Move to akasha packaging

---
+++ src/main/java/akasha/core/ArrayBufferView.java
@@ -0,0 +1,15 @@
+package akasha.core;
+
+import akasha.BufferSource;
+import javax.annotation.Generated;
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsType;
+
+@Generated("org.realityforge.webtack")
+@JsType(
+    isNative = true,
+    namespace = JsPackage.GLOBAL,
+    name = "?"
+)
+public interface ArrayBufferView extends BufferSource {
+}
